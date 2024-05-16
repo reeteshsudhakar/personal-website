9719eaa1e46b32062ab11ab6348fda35899149a3
Reetesh Sudhakar <rsudhakar9@gatech.edu>
2024-05-15 17:44:23 -0700

    home page, resume, and constants file updates

--- app/(home)/AboutHero/AboutHero.tsx
+++ app/(home)/AboutHero/AboutHero.tsx
@@ -2,10 +2,13 @@
 
 import { Text, Center, Stack } from '@mantine/core';
 import classes from './AboutHero.module.css';
+import { useMediaQuery } from '@mantine/hooks';
 
 export function AboutHero() {
+    const isLargeScreen = useMediaQuery('(min-width: 48em)');
+
     return (
-        <Center className={classes.wrapper}>
+        <Center className={isLargeScreen ? classes.wrapper : classes.wrapperSmall}>
             <Stack align='center' justify='center'>
                 <Text
                     className={classes.title}
--- app/(home)/ContactHero/ContactHero.tsx
+++ app/(home)/ContactHero/ContactHero.tsx
@@ -3,10 +3,13 @@
 import { Text, Center, Stack, Button, Group, Anchor } from '@mantine/core';
 import classes from './ContactHero.module.css';
 import { IconForms, IconCalendar, IconMail, IconBrandInstagram } from '@tabler/icons-react';
+import { useMediaQuery } from '@mantine/hooks';
 
 export function ContactHero() {
+    const isLargeScreen = useMediaQuery('(min-width: 48em)');
+
     return (
-        <Center className={classes.wrapper}>
+        <Center className={isLargeScreen ? classes.wrapper : classes.wrapperSmall}>
             <Stack align='center' justify='center'>
                 <Text
                     className={classes.title}
