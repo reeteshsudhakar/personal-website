ef75e570cc78d0b9e36b51938fd7cfec68b57706
Reetesh Sudhakar <rsudhakar9@gatech.edu>
2024-05-27 17:18:14 -0700

    fix to make nav visible on top of resumé

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,30 +1,47 @@
-'use client'
+"use client";
 
-import AppWrapper from "@/components/AppWrapper/AppWrapper";
 import { useMediaQuery } from "@mantine/hooks";
 
 export default function HomePage() {
-    const isLargeScreen = useMediaQuery('(min-width: 48em)');
+    const isLargeScreen = useMediaQuery("(min-width: 48em)");
 
     return (
-        <AppWrapper>
-            <>
-                {isLargeScreen ? (
-                    <iframe
-                        src="/resume.pdf"
-                        style={{ position: 'fixed', top: 0, bottom: 0, right: 0, left: 275, width: 'calc(100% - 275px)', border: 'none', margin: 0, padding: 0, overflow: 'hidden', zIndex: 999999, height: '100%' }}
-                    >
-                    </iframe>
-
-                ) : (
-                    <iframe
-                        src="/resume.pdf"
-                        style={{ position: 'fixed', top: 60, bottom: 0, right: 0, left: 0, width: '100%', border: 'none', margin: 0, padding: 0, overflow: 'hidden', zIndex: 999999, height: '100%' }}
-                    >
-                    </iframe>
-
-                )}
-            </>
-        </AppWrapper>
+        <>
+            {isLargeScreen ? (
+                <iframe
+                    src="/resume.pdf"
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        bottom: 0,
+                        right: 0,
+                        left: 275,
+                        width: "calc(100% - 275px)",
+                        border: "none",
+                        margin: 0,
+                        padding: 0,
+                        overflow: "hidden",
+                        height: "100%",
+                    }}
+                ></iframe>
+            ) : (
+                <iframe
+                    src="/resume.pdf"
+                    style={{
+                        position: "absolute",
+                        top: 60,
+                        bottom: 0,
+                        right: 0,
+                        left: 0,
+                        width: "100%",
+                        border: "none",
+                        margin: 0,
+                        padding: 0,
+                        overflow: "hidden",
+                        height: "100%",
+                    }}
+                ></iframe>
+            )}
+        </>
     );
 }
